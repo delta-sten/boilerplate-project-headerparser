f9b165b9a05ba251d8879b2b89ef41d5a7e598f8
Sten Schmidt <sten.schmidt@uscca.com>
2024-06-17 20:09:05 -0500

    j

--- index.js
+++ index.js
@@ -20,11 +20,13 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
+/*
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
+*/
 
-app.get('api/whoami', (req, res) => {
+app.get('/api/whoami', (req, res) => {
   res.json({
     ipaddress: req.socket.remoteAddress,
     language: req.headers['accept-language'],
